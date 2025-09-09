59053d71cca768503447b534fec0e67036475341
Teke <tekevics@gmail.com>
2025-09-09 20:58:12 +0200

    Test - NotFoundExceptionTest javitas

--- src/test/java/com/nyilvt/nyilvt/exception/NotFoundExceptionTest.java
+++ src/test/java/com/nyilvt/nyilvt/exception/NotFoundExceptionTest.java
@@ -1,14 +1,10 @@
 package com.nyilvt.nyilvt.exception;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
-
 class NotFoundExceptionTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
