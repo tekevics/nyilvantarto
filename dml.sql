29aaf0a80c2e1b2baf4c22b588709d89143d0ff4
Teke <tekevics@gmail.com>
2025-09-09 11:14:57 +0200

    DML file javitas

--- dml.sql
+++ dml.sql
@@ -2,12 +2,12 @@ TRUNCATE TABLE elerhetoseg;
 TRUNCATE TABLE cim;
 TRUNCATE TABLE szemely;
 
-INSERT INTO szemelyek (nev, szuletesi_hely) VALUES ('Nagy Péter', 'Budapest');
-INSERT INTO szemelyek (nev, szuletesi_hely) VALUES ('Kovács Anna', 'Debrecen');
+INSERT INTO szemely (nev, szuletesi_hely) VALUES ('Nagy Péter', 'Budapest');
+INSERT INTO szemely (nev, szuletesi_hely) VALUES ('Kovács Anna', 'Debrecen');
 
-INSERT INTO cimek (utca, varos, iranyitoszam, tipus, szemely_id) VALUES ('Fő utca 1.', 'Budapest', '1000', 'ALLANDO', 1);
-INSERT INTO cimek (utca, varos, iranyitoszam, tipus, szemely_id) VALUES ('Kossuth tér 10.', 'Szeged', '6720', 'IDEIGLENES', 1);
+INSERT INTO cim (utca, varos, iranyitoszam, tipus, szemely_id) VALUES ('Fő utca 1.', 'Budapest', '1000', 'ALLANDO', 1);
+INSERT INTO cim (utca, varos, iranyitoszam, tipus, szemely_id) VALUES ('Kossuth tér 10.', 'Szeged', '6720', 'IDEIGLENES', 1);
 
-INSERT INTO elerhetosegek (tipus, ertek, szemely_id) VALUES ('EMAIL', 'peter.nagy@email.hu', 1);
-INSERT INTO elerhetosegek (tipus, ertek, szemely_id) VALUES ('TELEFON', '+36701234567', 1);
-INSERT INTO elerhetosegek (tipus, ertek, szemely_id) VALUES ('EMAIL', 'anna.kovacs@email.com', 2);
+INSERT INTO elerhetoseg (tipus, ertek, szemely_id) VALUES ('EMAIL', 'peter.nagy@email.hu', 1);
+INSERT INTO elerhetoseg (tipus, ertek, szemely_id) VALUES ('TELEFON', '+36701234567', 1);
+INSERT INTO elerhetoseg (tipus, ertek, szemely_id) VALUES ('EMAIL', 'anna.kovacs@email.com', 2);
